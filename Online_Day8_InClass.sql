bf5740f53987aac8b25a19898657bc1f9ce6c0c4
Anishwar Ammayappan <anishwarau@gmail.com>
2020-07-08 19:23:33 +0530

    Add files via upload

---
+++ Online_Day8_InClass.sql
@@ -0,0 +1,19 @@
+Use Day7;
+show tables;
+# Question 1:
+# 1)  Display  records of “SAVINGS” account linked with “Credit cards" account_type and its credit aggregate sum of transaction amount. 
+
+# Question 2:
+# 2) Compare the aggregate transaction amount of current month versus aggregate transaction with previous months.
+# Display account_number, transaction_amount , 
+-- sum of current month transaction amount ,
+-- current month transaction date , 
+-- sum of previous month transaction amount , 
+-- previous month transaction date.
+ 
+# Question 3:
+# 3) Display individual accounts absolute transaction of every next  month is greater than the previous months .
+ 
+# Question 4:
+# 4) Find the no. of transactions of credit cards including add-on Credit Cards
+
---
+++ Online_Day8_Takehome.sql
@@ -0,0 +1,18 @@
+# Pre-requisite Use bank schema to solve the queries.alter
+
+# Question 1:
+# 1) Print Savings Account number where its corresponding Credit cards including AddonCredit card transactions for more than one time .
+
+# Question 2:
+# 2) Print Savings Account number where its corresponding only “Addon” - Credit cards  transactions for at least once.
+
+
+# Question 3:
+# 3) Print the customer_id and length of customer_id using Natural join on 
+# Tables :  bank_customer  , bank_customer_export 
+# Note: Do not use table alias to refer to column names.
+
+# Question 4:
+# 4) Print customer_id, customer_name and other common columns  from both 
+-- the Tables :  bank_customer & bank_customer_export without missing any matching customer_id key column records. 
+-- Note: refer datatype conversion if found any missing records
